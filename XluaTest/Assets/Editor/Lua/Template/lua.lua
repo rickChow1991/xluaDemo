cb9d3d3e0e67c66e67dd44ee7273d6f53fb4aa86
rickChow1991 <346239285@qq.com>
2017-12-18 21:39:39 +0800

    update

---
+++ XluaTest/Assets/Editor/Lua/Template/lua.lua
@@ -0,0 +1,12 @@
+--[[--
+Author:#Author#
+CreateTime:#CreateTime#
+Description:#Description#
+]]
+local M = {}
+M._moduleName = ...
+M.__index = M
+----- begin module -----
+
+----- end -----
+return M
